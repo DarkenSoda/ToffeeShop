2ab214bcc75f45e61d71108a9125892a033e9eb4
Nour8317 <noure8317@gmail.com>
2023-05-05 18:47:45 +0300

    Customer Class

---
+++ UsersClasses/Customer.cs
@@ -0,0 +1,46 @@
+using CS251_A3_ToffeeShop.CartClasses;
+using CS251_A3_ToffeeShop.BalanceClasses;
+namespace CS251_A3_ToffeeShop.UsersClasses
+{
+    public class Customer : User
+    {
+        public string? customerID;
+        List<Order>? orderHistory;
+        ShoppingCart? shoppingCart;
+        List<Voucher>? vouchers;
+        LoyalityPoints? loyalityPoints;
+        //List<PaymentMethodstrategy>? PaymentMethods;
+        Address? address;
+
+        Customer(string name, string userName, string password, string emailAdress) : base(name, userName, password, emailAdress) { }
+        public ShoppingCart GetShoppingCart()
+        {
+            // Console.WriteLine("1-Add Item.");
+            // Console.WriteLine("2-Remove Item.");
+            // Console.WriteLine("3-Update Item.");
+            // Console.WriteLine("4-Clear Cart.");
+            // Console.WriteLine("Enter Your Choice: ");
+
+            // int y = Convert.ToInt32(Console.ReadLine());
+            // if (y == 1)
+            // {
+            //     shoppingCart.PrintItems();
+            //     //string name, string category, double price = 0
+            //     string? name;
+            //     string? category;
+            //     double? price = 0;
+            //     Console.WriteLine("Enter The Product")
+            //     Console.WriteLine("Enter The Product")
+            //     Console.WriteLine("Enter The Product")
+            //     Console.WriteLine("Enter The Product")
+            //     shoppingCart.AddItem()
+            return shoppingCart;
+        }
+        public List<Order> GetOrderHistory(){
+            return orderHistory;
+        }
+        public Address GetAddress(){
+            return address;
+        }
+    }
+}
--- UsersClasses/Staff.cs
+++ UsersClasses/Staff.cs
@@ -3,7 +3,7 @@ namespace CS251_A3_ToffeeShop.UsersClasses {
     public class Staff : User{
         public Staff(string name, string userName, string password, string emailAdress):base(name,userName,password,emailAdress){}
         public void UpdataOrder(Order order){
-
+            
         }
     }
 }
