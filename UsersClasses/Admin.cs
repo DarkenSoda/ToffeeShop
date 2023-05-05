133bccdf35c5fe7bb4709dca02f12291b2a5d9c8
Nour8317 <noure8317@gmail.com>
2023-05-05 20:15:18 +0300

    some function in customer class

--- UsersClasses/Admin.cs
+++ UsersClasses/Admin.cs
@@ -88,5 +88,31 @@ namespace CS251_A3_ToffeeShop.UsersClasses
         {
             order.orderStatus = OrderState.Canceled;
         }
+        public void UpdataOrder(Order order){
+            Console.WriteLine("1-Update Order State.");
+            Console.WriteLine("2-Update Order Address.");
+            int y;
+            y = Convert.ToInt32(Console.ReadLine());
+            if (y==1)
+            {
+                order.UpdateState();
+            }else if (y==2)
+            {
+                string street;
+                string city;
+                string buildingNumber;
+                Console.WriteLine("Enter The Street Name: ");
+                street = Console.ReadLine();
+                Console.WriteLine("Enter The City: ");
+                city = Console.ReadLine();
+                Console.WriteLine("Enter The Building Number: ");
+                buildingNumber = Console.ReadLine();
+                Address newaddress = new Address(street,city,buildingNumber);
+                order.SetAddress(newaddress);
+            }
+        }
+        public void SuspendCustomer(Customer customer){
+            //we have to but flag or statue in customer class to know if the customer is blocked or not
+        }
     }
 }
