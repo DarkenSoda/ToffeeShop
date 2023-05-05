b67dab20dcead2d18489b3e771bd96f0e34621fc
DarkenSoda <mahmoudadel200190@yahoo.com>
2023-05-05 17:16:32 +0300

    Email OTP verification complete

--- PaymentMethod/OnDeliveryStrategy.cs
+++ PaymentMethod/OnDeliveryStrategy.cs
@@ -16,11 +16,30 @@ namespace CS251_A3_ToffeeShop.PaymentMethod {
             // Generate random number
             int OTP = GenerateRandomNumber();
 
-            // Create smtp connection and send Message
-            // Note: Suffering
-            
+            try {
+                // Create smtp connection and send Message
+                SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+                MailMessage message = new MailMessage();
+                message.From = new MailAddress("fcai.toffeeshop@gmail.com");
+                message.To.Add(new MailAddress(emailAdress));
+                message.Subject = "Your verifcation OTP from Toffee Shop!";
+                message.Body = $"<div>Your one time password verifcation is <h2>{OTP}</h2></div>";
+                message.IsBodyHtml = true;
+
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
+                smtp.Credentials = new NetworkCredential("fcai.toffeeshop@gmail.com", "dfpzbhgihyfxtbjp");
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+                smtp.Send(message);
+            } catch {
+                Console.WriteLine("Failed to send OTP!");
+                return false;
+            }
+
             // Validate user Input
             int input;
+            Console.WriteLine("Please Check your spam if you can't find a message in your inbox!");
             Console.Write("Please Enter OTP sent to the provided email address: ");
             int.TryParse(Console.ReadLine(), out input);
 
